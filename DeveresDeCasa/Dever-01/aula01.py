5bab37f38878e23d3299786199f03d2c78243d6a
Ivin <vaz.ivin@gmail.com>
2025-02-24 19:43:13 -0300

    corrigindo código com base na diferença de diretório

--- DeveresDeCasa/Dever-01/aula01.py
+++ DeveresDeCasa/Dever-01/aula01.py
@@ -2,7 +2,7 @@ import csv
 import pandas
 from datetime import datetime
 
-data = pandas.read_csv("aprendizagem-de-maquina\Aula01\Data_aula01.csv") #Lê arquivos com a biblioteca pandas
+data = pandas.read_csv("aprendizagem-de-maquina\DeveresDeCasa\Dever-01\Data_aula01.csv") #Lê arquivos com a biblioteca pandas
 coluna = int(input("Qual registro você deseja?"))-1 #Cria um input com o número de registro desejado
 
 data_list = (data["Data_Nascimento"][coluna]).split("-") #recebe o registro referente ao solicitado e o divide em uma lista pelos "-"
