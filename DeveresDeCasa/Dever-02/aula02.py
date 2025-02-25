586ac4db2bc4feb5e7c9874e4e077658c33708ef
Ivin <vaz.ivin@gmail.com>
2025-02-24 21:56:01 -0300

    Adiconando parte do dever de casa

---
+++ DeveresDeCasa/Dever-02/aula02.py
@@ -0,0 +1,12 @@
+#1
+import fazer_str_com_conteudo from "aprendizagem-de-maquina\Funcoes"
+frase = fazer_str_com_conteudo()
+
+#2
+caracteres_totais = len(list(frase))
+palavras_totais = (list(frase)).split()
+print(len(palavras_totais))
+maior_palavra = max(palavras_totais)
+#3
+
+#4
