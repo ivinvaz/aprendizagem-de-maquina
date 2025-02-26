c25e5f88e6c9c584d6d031c37f91f2cf6a64308a
Ivin <vaz.ivin@gmail.com>
2025-02-26 20:10:35 -0300

    Dever de casa 02

--- DeveresDeCasa/Dever-02/aula02.py
+++ DeveresDeCasa/Dever-02/aula02.py
@@ -1,12 +1,48 @@
-#1
-import fazer_str_com_conteudo from "aprendizagem-de-maquina\Funcoes"
+#OBS, não estava conseguindo importar a função do arquivo Funcoes de jeito nenhum.
+
+def fazer_str_com_conteudo():
+  frase = input("Frase: ")
+  errado = True
+  while errado:
+    if frase == "":
+        raise TypeError("A frase não pode ser vazia")
+        frase = input("Frase: ")
+        return frase
+    else:
+        errado = False
+        return frase
+
+#1 
+
 frase = fazer_str_com_conteudo()
+lista_palavras = frase.split()
+lista_frase = list(frase)
 
 #2
-caracteres_totais = len(list(frase))
-palavras_totais = (list(frase)).split()
-print(len(palavras_totais))
-maior_palavra = max(palavras_totais)
+
+caracteres_totais = [len(palavras) for palavras in lista_palavras]
+caracteres_totais = sum(caracteres_totais)
+
+quantas_palavras = len(lista_palavras)
+
+maior_palavra = ""
+for palavras in lista_palavras:
+    if len(palavras) > len(maior_palavra):
+        maior_palavra = palavras
+
 #3
 
+frase_ao_contrario = lista_frase[::-1]
+frase_ao_contrario = " ".join(frase_ao_contrario)
+
+
+frase_contraria = lista_palavras[::-1]
+frase_contraria = " ".join(frase_contraria)
+
+frase_maiuscula = frase.upper()
+
+frase_tupla = tuple(lista_palavras)
+
 #4
+
+print(f"-Número de caracteres da frase: {caracteres_totais}\n-Número de palavras: {quantas_palavras}\n-A palavra com maior comprimento: {maior_palavra}\n-A frase invertida por caracteres: {frase_ao_contrario}\n-A frase invertida por palavras:{frase_contraria}\n-A frase em maiúsculas e minúsculas: {frase_maiuscula}\n-A tupla formada pelas palavras da frase: {frase_tupla}")
