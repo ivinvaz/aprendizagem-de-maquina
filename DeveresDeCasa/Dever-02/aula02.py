e7817295261495053566743ab9d048c281d9ab64
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-03-07 16:56:29 -0300

    otimizando código do dever02

--- DeveresDeCasa/Dever-02/aula02.py
+++ DeveresDeCasa/Dever-02/aula02.py
@@ -25,19 +25,14 @@ caracteres_totais = sum(caracteres_totais)
 
 quantas_palavras = len(lista_palavras)
 
-maior_palavra = ""
-for palavras in lista_palavras:
-    if len(palavras) > len(maior_palavra):
-        maior_palavra = palavras
+maior_palavra = max(lista_palavras, key=len)
 
 #3
 
-frase_ao_contrario = lista_frase[::-1]
+frase_ao_contrario = [palavras[::-1] for palavras in lista_palavras]
 frase_ao_contrario = " ".join(frase_ao_contrario)
 
-
-frase_contraria = lista_palavras[::-1]
-frase_contraria = " ".join(frase_contraria)
+frase_contraria = " ".join(reversed(lista_palavras))
 
 frase_maiuscula = frase.upper()
 
